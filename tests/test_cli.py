588962ace9258d0b914804689646c84e24d8f426
Dewet Diener <dewet22@users.noreply.github.com>
2021-12-30 13:55:47 +0000

    add CLI test

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+from click.testing import CliRunner
+
+from givenergy_modbus import cli
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'givenergy-modbus' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
