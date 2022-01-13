57288ac1280157993e181a7c837b8fdc5e405f3d
Dewet Diener <dewet22@users.noreply.github.com>
2022-01-13 23:44:06 +0000

    start adding more cli commands

--- givenergy_modbus/cli.py
+++ givenergy_modbus/cli.py
@@ -1,5 +1,4 @@
 """Console script for interacting with GivEnergy inverters."""
-
 import logging
 
 import click
@@ -11,33 +10,47 @@ from givenergy_modbus.util import InterceptHandler
 _logger = logging.getLogger(__package__)
 
 
+def is_documented_by(original):
+    """Copies the docstring from the original source to the decorated target."""
+
+    def wrapper(target):
+        target.__doc__ = original.__doc__
+        return target
+
+    return wrapper
+
+
 @click.group()
+@click.argument('host', type=str)
 @click.option(
     '--log-level',
     default='INFO',
     type=click.Choice(['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'], case_sensitive=False),
 )
-def main(log_level):
+@click.pass_context
+def main(ctx, host, log_level):
     """A python library to access GivEnergy inverters via Modbus TCP, with no dependency on the GivEnergy Cloud."""
+    ctx.ensure_object(dict)
+    click.echo(host)
+
     # Install our improved logging handler.
     logging.basicConfig(handlers=[InterceptHandler()], level=getattr(logging, log_level))
+    ctx.obj['CLIENT'] = GivEnergyClient(host=host)
 
 
 @main.command()
-@click.option('-h', '--host', type=str)
+@click.pass_context
 @click.option('-b', '--batteries', type=int, default=1)
-def dump_registers(host, batteries):
+def dump_registers(ctx, batteries):
     """Dump out raw register data for use in debugging."""
-    _logger.info(f'Connecting to host {host}')
-    client = GivEnergyClient(host=host)
     rc = RegisterCache()
-    client.update_inverter_registers(register_cache=rc)
+    ctx.obj['CLIENT'].update_inverter_registers(register_cache=rc)
     inverter_json = rc.to_json()
 
     batteries_json = {}
     for i in range(batteries):
         rc = RegisterCache()
-        client.update_battery_registers(register_cache=rc, battery_number=i)
+        ctx.obj['CLIENT'].update_battery_registers(register_cache=rc, battery_number=i)
         batteries_json[i] = rc.to_json()
 
     click.echo('Inverter registers:')
@@ -46,5 +59,20 @@ def dump_registers(host, batteries):
     click.echo(batteries_json)
 
 
+@main.command()
+@click.pass_context
+@click.argument('target_soc', type=int)
+@is_documented_by(GivEnergyClient.enable_charge_target)
+def enable_charge_target(ctx, target_soc):  # noqa: D103
+    ctx.obj['CLIENT'].enable_charge_target(target_soc)
+
+
+@main.command()
+@click.pass_context
+@is_documented_by(GivEnergyClient.disable_charge_target)
+def disable_charge_target(ctx):  # noqa: D103
+    ctx.obj['CLIENT'].disable_charge_target()
+
+
 if __name__ == "__main__":
-    main()  # pragma: no cover
+    main(obj={})  # pragma: no cover
