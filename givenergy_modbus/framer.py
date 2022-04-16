777d7dce3a027de4141959f2b1ede731ec8fd04b
dewet22 <dewet22@users.noreply.github.com>
2022-04-16 18:55:20 +0100

    Bugfix - extract the correct frame (an overfull buffer will include the following frame's opening "YY") for correctness (PDU decoding coped fine, but added a check for it regardless)

--- givenergy_modbus/framer.py
+++ givenergy_modbus/framer.py
@@ -145,7 +145,7 @@ class GivEnergyModbusFramer:
 
     def get_frame(self):
         """Extract the next PDU frame from the buffer, removing the MBAP header except for the function id."""
-        return self._buffer[self.FRAME_HEAD_SIZE - 1 : self.FRAME_HEAD_SIZE + self._length]
+        return self._buffer[self.FRAME_HEAD_SIZE - 1: self.FRAME_HEAD_SIZE + self._length - 2]
 
     def process_incoming_packet(self, data: bytes, callback: Callable) -> None:
         """Process an incoming packet.
