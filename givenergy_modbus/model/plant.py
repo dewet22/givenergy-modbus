af1588810454b4bf7a0f0ea036597759768b9db7
dewet22 <dewet22@users.noreply.github.com>
2022-04-16 18:23:32 +0100

    Refactor Plant to care about slave addresses, and allow updating from received PDU Responses directly

--- givenergy_modbus/model/plant.py
+++ givenergy_modbus/model/plant.py
@@ -1,37 +1,51 @@
-from typing import List
+import logging
+from typing import List, Mapping, Dict
 
 from pydantic import BaseModel
 
 from givenergy_modbus.model.battery import Battery
 from givenergy_modbus.model.inverter import Inverter  # type: ignore  # shut up mypy
+from givenergy_modbus.model.register import HoldingRegister
 from givenergy_modbus.model.register_cache import RegisterCache
+from givenergy_modbus.pdu import ModbusPDU, ReadRegistersResponse, ReadHoldingRegistersResponse, \
+    ReadInputRegistersResponse
+
+_logger = logging.getLogger(__package__)
 
 
 class Plant(BaseModel):
     """Representation of a complete GivEnergy plant."""
 
-    inverter_rc: RegisterCache
-    batteries_rcs: List[RegisterCache]
+    register_caches: Dict[int, RegisterCache] = {}
+    number_batteries: int = 0
 
     class Config:  # noqa: D106
         arbitrary_types_allowed = True
         orm_mode = True
         # allow_mutation = False
 
-    def __init__(self, **data):
+    def __init__(self, *args, **kwargs):
         """Constructor. Use `number_batteries` to specify the total number of batteries installed."""
-        data['inverter_rc'] = data.get('inverter_rc', RegisterCache())
-        data['batteries_rcs'] = data.get(
-            'batteries_rcs', [RegisterCache() for _ in range(data.get('number_batteries', 0))]
-        )
-        super().__init__(**data)
+        super().__init__(*args, **kwargs)
+        # ensure expected RegisterCaches are populated
+        if not self.register_caches:
+            self.register_caches = {0x32: RegisterCache(0x32)}
+        if self.number_batteries > 1:
+            for i in range(self.number_batteries-1):
+                self.register_caches[i+0x33] = RegisterCache(i+0x33)
+
+    def update(self, pdu: ModbusPDU):
+        if pdu.slave_address not in self.register_caches:
+            _logger.warning(f'Unexpected slave address {hex(pdu.slave_address)}')
+            self.register_caches[pdu.slave_address] = RegisterCache(slave_address=pdu.slave_address)
+        self.register_caches[pdu.slave_address].update_from_pdu(pdu)
 
     @property
     def inverter(self) -> Inverter:
         """Return Inverter model for the Plant."""
-        return Inverter.from_orm(self.inverter_rc)
+        return Inverter.from_orm(self.register_caches[0x32])
 
     @property
     def batteries(self) -> List[Battery]:
         """Return Battery models for the Plant."""
-        return [Battery.from_orm(rc) for rc in self.batteries_rcs]
+        return [Battery.from_orm(self.register_caches[i + 0x32]) for i in range(self.number_batteries)]
