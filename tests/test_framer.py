4e6bbaf858e7f9bdc88f722bf57dd5ed23f6dcfb
dewet22 <dewet22@users.noreply.github.com>
2022-04-22 13:11:36 +0100

    Refactor and simplify Framer
    pymodbus enforces an unnecessarily complicated contract between the parts of the framer and decoders. This removes the dependency and hopefully makes it simpler to understand.
    Make tests much clearer

--- tests/test_framer.py
+++ tests/test_framer.py
@@ -1,15 +1,16 @@
 """Tests for GivEnergyModbusFramer."""
 import sys
-from typing import Any, Dict, Tuple
 from unittest.mock import MagicMock, call
 
 import pytest
 from pymodbus.framer.socket_framer import ModbusSocketFramer
 
-from givenergy_modbus.decoder import GivEnergyRequestDecoder, GivEnergyResponseDecoder
-from givenergy_modbus.framer import GivEnergyModbusFramer
-from givenergy_modbus.pdu import HeartbeatRequest, ReadInputRegistersResponse
-from tests import REQUEST_PDU_MESSAGES, RESPONSE_PDU_MESSAGES, _lookup_pdu_class
+from givenergy_modbus.framer import ClientFramer, Framer, ServerFramer
+from givenergy_modbus.pdu import BasePDU
+from givenergy_modbus.pdu.heartbeat import HeartbeatRequest
+from givenergy_modbus.pdu.null import NullResponse
+from givenergy_modbus.pdu.read_registers import ReadHoldingRegistersResponse, ReadInputRegistersResponse
+from tests import ALL_MESSAGES, CLIENT_MESSAGES, SERVER_MESSAGES, PduTestCaseSig, _lookup_pdu_class
 
 VALID_REQUEST_FRAME = (  # actual recorded request frame, look up 6 input registers starting at #0
     b"\x59\x59\x00\x01\x00\x1c\x01\x02"  # 7-byte MBAP header + function code
@@ -55,299 +56,180 @@ EXCEPTION_RESPONSE_FRAME = (  # actual recorded response frame, to request above
 
 def test_framer_constructor():
     """Test constructor."""
-    client_decoder = MagicMock()
-    framer = GivEnergyModbusFramer(client_decoder)
+    framer = Framer()
+    assert not isinstance(framer, ModbusSocketFramer)
+    framer.decoder = MagicMock()
     assert framer.FRAME_HEAD == ">HHHBB"
     assert framer.FRAME_HEAD_SIZE == 0x08
     assert framer._buffer == b""
     assert not hasattr(framer, '_length')
     assert framer.buffer_length == 0
-    assert framer.decoder == client_decoder
-    client_decoder.assert_not_called()
-
-    assert not isinstance(framer, ModbusSocketFramer)
+    framer.decoder.assert_not_called()
 
 
-@pytest.fixture
-def requests_framer():
-    """Yield a real framer for processing Request messages."""
-    yield GivEnergyModbusFramer(GivEnergyRequestDecoder())
-
-
-@pytest.fixture
-def responses_framer():
-    """Yield a real framer for processing Response messages."""
-    yield GivEnergyModbusFramer(GivEnergyResponseDecoder())
-
-
-@pytest.mark.parametrize(
-    "data",  # [(input_buffer, complete, header_valid, expected_length, expected_remaining_buffer), (..), ..]
-    [  # list[Tuple[bytes, bool, bool, int, bytes]]
-        (  # data0 - no data
-            b"",
-            False,
-            True,
-            -1,
-            b"",
-        ),
-        (  # data1 - incomplete frame, length=0xcc but not enough data follows
-            b"\x02\x01\x01\x00Q\xcc",
-            False,
-            True,
-            -1,
-            b"\x02\x01\x01\x00Q\xcc",
-        ),
-        (  # data2 - incomplete frame, length=0x9e but not enough data follows
-            b"\x59\x59\x00\x01\x00\x9e\x01",
-            False,
-            True,
-            -1,
-            b"\x59\x59\x00\x01\x00\x9e\x01",
-        ),
-        (  # data3 - valid frame, length=1
-            b"\x59\x59\x00\x01\x00\x01\x01\x02",
-            True,
-            True,
-            1,
-            b"\x02",
-        ),
-        (  # data4 - valid frame, fn_id 0x01 is Transparent
-            b"\x59\x59\x00\x01\x00\x0d\x01\x01\x57\x46\x32\x31\x32\x35\x47\x33\x31\x36\x01",
-            True,
-            True,
-            13,
-            b"",
-        ),
-        (  # data5 - no valid frame, don't strip leading trash until next frame visible
-            b"\x59\x59\x59\x59\x00\x05\x59\x59\x59\x59\x59\x41\x42",
-            False,
-            False,
-            -1,
-            b"\x59\x59\x59\x59\x00\x05\x59\x59\x59\x59\x59\x41\x42",
-        ),
-        (  # data6 - valid frame, length=2 with trailing buffer contents
-            b"\x59\x59\x00\x01\x00\x02\x01\x02\xff\x01\x02\x03",
-            True,
-            True,
-            2,
-            b"\xff\x01\x02\x03",
-        ),
-        (  # data7 - invalid MBAP, don't strip leading trash until next frame visible
-            b"\x00\x01\x12\x34\x00\x04\xff\x02\x12\x34",
-            True,
-            False,
-            4,
-            b"\x00\x01\x12\x34\x00\x04\xff\x02\x12\x34",
-        ),
-        (  # data8 - VALID_REQUEST_FRAME with trailing data
-            VALID_REQUEST_FRAME + b"\xde\xad\xbe\xef",
-            True,
-            True,
-            28,
-            b"\xde\xad\xbe\xef",
-        ),
-        (  # data9 - VALID_RESPONSE_FRAME with trailing data
-            VALID_RESPONSE_FRAME + b"\x01\x02\x03",
-            True,
-            True,
-            50,
-            b"\x01\x02\x03",
-        ),
-        (  # data10 - exception response
-            EXCEPTION_RESPONSE_FRAME,
-            True,
-            True,
-            38,
-            b"",
-        ),
-        (  # data11 - exception response + valid response
-            EXCEPTION_RESPONSE_FRAME + VALID_RESPONSE_FRAME,
-            True,
-            True,
-            38,
-            VALID_RESPONSE_FRAME,
-        ),
-        (  # data12 - junk + valid response
-            b'\x01\x02\x03\x04\x05\x06\x00\x01\x02\x03\x04\x05\x06\x00\x01\x02\x03\x04\x05\x06' + VALID_RESPONSE_FRAME,
-            True,
-            False,
-            38,
-            VALID_RESPONSE_FRAME,
-        ),
-    ],
-)
-def test_check_frame(requests_framer, data: Tuple[bytes, bool, bool, int, bytes]):
-    """Validate the internal state of the framer as data gets processed."""
-    input_buffer, is_complete_frame, is_valid_frame, expected_length, expected_remaining_buffer = data
-
-    assert requests_framer.is_frame_ready() is False
-    assert requests_framer.check_frame() is False
-    assert requests_framer.buffer_length == 0
-    assert not hasattr(requests_framer, '_length')
-
-    requests_framer.add_to_frame(input_buffer)
-    assert requests_framer.buffer_length == len(input_buffer)
-
-    if is_valid_frame:
-        assert requests_framer.check_frame() == is_complete_frame
-        if expected_length >= 0:
-            assert requests_framer._length == expected_length
-        else:
-            assert not hasattr(requests_framer, '_length')
-
-        if is_complete_frame:
-            requests_framer.advance_frame()
-    else:
-        assert not requests_framer.check_frame()
-        assert not hasattr(requests_framer, '_length')
-
-    assert requests_framer._buffer == expected_remaining_buffer
-
-
-@pytest.mark.parametrize("data", REQUEST_PDU_MESSAGES)
-def test_request_wire_encoding(requests_framer, data: Tuple[str, Dict[str, Any], bytes, bytes, Exception]):
-    """Ensure Request PDU messages can be encoded to the correct wire format."""
-    pdu_fn, pdu_fn_kwargs, mbap_header, encoded_pdu, ex = data
-
-    pdu = _lookup_pdu_class(pdu_fn)(**pdu_fn_kwargs)
+@pytest.mark.parametrize(PduTestCaseSig, ALL_MESSAGES)
+def test_encoding(str_repr, pdu_class_name, constructor_kwargs, mbap_header, inner_frame, ex):
+    """Ensure message objects can be encoded to the correct wire format."""
+    framer = Framer()
+    pdu = _lookup_pdu_class(pdu_class_name)(**constructor_kwargs)
     if ex:
         with pytest.raises(ex.__class__) as e:
-            requests_framer.build_packet(pdu)
+            framer.build_packet(pdu)
         assert e.value.args[0] == ex.args[0]
         assert e.value.args[1] == pdu
     else:
-        packet = requests_framer.build_packet(pdu)
-        assert packet == mbap_header + encoded_pdu
+        packet = framer.build_packet(pdu)
+        assert packet.hex() == (mbap_header + inner_frame).hex()
 
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher")
-@pytest.mark.parametrize("data", REQUEST_PDU_MESSAGES)
-def test_request_wire_decoding(requests_framer, data: Tuple[str, Dict[str, Any], bytes, bytes, Exception]):
+@pytest.mark.parametrize(PduTestCaseSig, SERVER_MESSAGES)
+def test_server_decoding(str_repr, pdu_class_name, constructor_kwargs, mbap_header, inner_frame, ex):
     """Ensure Request PDU messages can be decoded from raw messages."""
-    pdu_fn, pdu_fn_kwargs, mbap_header, encoded_pdu, ex = data
-
+    framer = ServerFramer()
     callback = MagicMock(return_value=None)
-    requests_framer.process_incoming_packet(mbap_header + encoded_pdu, callback)
+    framer.process_incoming_data(mbap_header + inner_frame, callback)
     if ex:
-        assert callback.mock_calls == [call.__bool__(), call(None, mbap_header + encoded_pdu)]
+        assert callback.mock_calls == [call((None, mbap_header + inner_frame))]
     else:
         callback.assert_called_once()
-        assert callback.mock_calls[0] == call.__bool__()
-        fn_kwargs = vars(callback.mock_calls[1].args[0])
-        for (key, val) in pdu_fn_kwargs.items():
-            assert fn_kwargs[key] == val
+        fn_kwargs = vars(callback.mock_calls[0].args[0][0])
+        for (key, val) in constructor_kwargs.items():
+            assert fn_kwargs[key] == val, f'{key} must match'
         assert fn_kwargs["data_adapter_serial_number"] == "AB1234G567"
         if hasattr(fn_kwargs, 'slave_address'):
             assert fn_kwargs["slave_address"] == 0x32
-            assert fn_kwargs["check"] == int.from_bytes(encoded_pdu[-2:], "big")
-
-
-@pytest.mark.parametrize("data", RESPONSE_PDU_MESSAGES)
-def test_response_wire_encoding(responses_framer, data: Tuple[str, Dict[str, Any], bytes, bytes]):
-    """Ensure Response PDU messages can be encoded to the correct wire format."""
-    pdu_fn, pdu_fn_kwargs, mbap_header, encoded_pdu = data
-
-    pdu = _lookup_pdu_class(pdu_fn)(**pdu_fn_kwargs)
-    packet = responses_framer.build_packet(pdu)
-    assert packet == mbap_header + encoded_pdu
+            assert fn_kwargs["check"] == int.from_bytes(inner_frame[-2:], "big")
 
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher")
-@pytest.mark.parametrize("data", RESPONSE_PDU_MESSAGES)
-def test_client_wire_decoding(responses_framer, data: Tuple[str, Dict[str, Any], bytes, bytes]):
+@pytest.mark.parametrize(PduTestCaseSig, CLIENT_MESSAGES)
+def test_client_decoding(str_repr, pdu_class_name, constructor_kwargs, mbap_header, inner_frame, ex):
     """Ensure Response PDU messages can be decoded from raw messages."""
-    pdu_fn, pdu_fn_kwargs, mbap_header, encoded_pdu = data
-
+    framer = ClientFramer()
     callback = MagicMock(return_value=None)
-    responses_framer.process_incoming_packet(mbap_header + encoded_pdu, callback)
+    framer.process_incoming_data(mbap_header + inner_frame, callback)
     callback.assert_called_once()
-    assert callback.mock_calls[0] == call.__bool__()
-    fn_kwargs = vars(callback.mock_calls[1].args[0])
-    for (key, val) in pdu_fn_kwargs.items():
+    fn_kwargs = vars(callback.mock_calls[0].args[0][0])
+    for (key, val) in constructor_kwargs.items():
         assert fn_kwargs[key] == val, f'`{key}` attribute must match'
-    assert fn_kwargs["data_adapter_serial_number"] == "WF1234G567"
-    if hasattr(fn_kwargs, 'slave_address'):
-        assert fn_kwargs["slave_address"] == 0x32
-        assert fn_kwargs["check"] == int.from_bytes(encoded_pdu[-2:], "big")
+    if 'check' in fn_kwargs:
+        assert fn_kwargs["check"] == int.from_bytes(inner_frame[-2:], "big")
 
 
-def test_process_heartbeat_request(responses_framer):
-    """Test error response processing."""
-    buffer = bytes.fromhex('5959 0001 000d 0101 5746 3132 3334 4735 3637 02')
-
+def decode(framer_class: type[Framer], buffer: str) -> BasePDU:
     callback = MagicMock(return_value=None)
-    responses_framer.process_incoming_packet(buffer, callback)
-
+    framer_class().process_incoming_data(bytes.fromhex(buffer), callback)
     callback.assert_called_once()
-    response = callback.call_args_list[0][0][0]
+    response = callback.call_args_list[0][0][0][0]
+    return response
+
+
+def test_process_heartbeat_request():
+    response = decode(ClientFramer, '5959 0001 000d 0101 5746 3132 3334 4735 3637 02')
     assert isinstance(response, HeartbeatRequest)
     assert not hasattr(response, 'function_code')
     assert response.data_adapter_serial_number == 'WF1234G567'
     assert response.data_adapter_type == 2
 
 
-def test_process_stream_short_frame(responses_framer):
-    """Test a buffer with a truncated message."""
-    buffer = bytes.fromhex(
-        '59 5900 0100 9e01 0257 4632 3132 3547 3331 3600 0000 0000 0000 '
-        '8a11 0353 4132 3131 3447 3034 3700 0000 3c20 0100 0308 3202 0100 00c3 500e 1000 0142 4732 3133 3447 3030 3753 '
-        '4132 3131 3447 3034 370b bd01 c100 0001 c100 0200 0080 0076 1b17 7000 0100 0000 0000 1100 0000 0400 0700 8c00 '
-        '1600 0100 0b00 0e00 0c00 3400 0100 0200 0000 0000 0000 6500 0100 0000 0000 6400'
+def test_process_null_response():
+    response = decode(
+        ClientFramer,
+        '5959 0001 009e 0102 5746 3132 3334 4735 3637 0000 0000 0000 008a 3200 0000 '
+        '0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 '
+        '0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 '
+        '0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 '
+        '0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 '
+        '0000 0000 0000 0000 0000 0000 0000',
     )
+    assert isinstance(response, NullResponse)
+    assert not hasattr(response, 'function_code')
+    assert response.data_adapter_serial_number == 'WF1234G567'
 
+
+def test_process_short_buffer():
+    """Test a buffer with a truncated message."""
+    framer = ClientFramer()
+    buffer = bytes.fromhex(
+        '5959 0001 009e 0102 5746 3231 3235 4733 3136 0000 0000 0000 008a 1103 5341'
+        '3231 3134 4730 3437 0000 003c 2001 0003 0832 0201 0000 c350 0e10 0001 4247'
+        '3231 3334 4730 3037 5341 3231 3134 4730 3437 0bbd 01c1 0000 01c1 0002 0000'
+        '8000 761b 1770 0001 0000 0000 0011 0000 0004 0007 008c 0016 0001 000b 000e'
+        '000c 0034 0001 0002 0000 0000 0000 0065 0001 0000 0000 0064 00'
+    )
     callback = MagicMock(return_value=None)
-    responses_framer.process_incoming_packet(buffer, callback)
+
+    framer.process_incoming_data(buffer, callback)
+
     callback.assert_not_called()
 
+    framer.process_incoming_data(buffer, callback)
+    assert len(callback.call_args_list) == 1
+    callback_args = callback.call_args_list[0][0][0]
+    response = callback_args[0]
+    assert isinstance(response, ReadHoldingRegistersResponse)
+    assert response.base_register == 0
+    assert response.register_count == 60
+    assert len(response.register_values) == 60
+    assert response.error is False
+    assert response.inner_function_code == 3
+    assert callback_args[1] == buffer + buffer[:19]
+    assert framer._buffer == buffer[19:]
 
-def test_process_stream_good(responses_framer):
+
+def test_process_stream_good():
     """Test a buffer of good messages without noise."""
+    framer = ClientFramer()
     buffer = EXCEPTION_RESPONSE_FRAME + VALID_RESPONSE_FRAME
-
     callback = MagicMock(return_value=None)
-    responses_framer.process_incoming_packet(buffer, callback)
+
+    framer.process_incoming_data(buffer, callback)
 
     assert len(callback.call_args_list) == 2
 
-    response = callback.call_args_list[0][0][0]
+    response = callback.call_args_list[0][0][0][0]
     assert isinstance(response, ReadInputRegistersResponse)
     assert response.base_register == 0
     assert response.register_count == 120
     assert response.register_values == []
     assert response.error is True
-    assert response.function_code == 0x04
+    assert response.inner_function_code == 0x04
 
-    response = callback.call_args_list[1][0][0]
+    response = callback.call_args_list[1][0][0][0]
     assert isinstance(response, ReadInputRegistersResponse)
     assert response.base_register == 0
     assert response.register_count == 6
     assert response.register_values == [1, 3054, 3029, 3881, 0, 2389]
     assert response.error is False
-    assert response.function_code == 0x04
+    assert response.inner_function_code == 0x04
 
 
-@pytest.mark.skip('FIXME return to this some day')
-def test_process_stream_good_but_noisy(responses_framer):
+def test_process_stream_good_but_noisy():
     """Test a buffer of good messages without noise."""
     buffer = b'\x01\x02asdf' + EXCEPTION_RESPONSE_FRAME + b'foobarbaz' + VALID_RESPONSE_FRAME + b'\x00\x99\xff'
-
     callback = MagicMock(return_value=None)
-    responses_framer.process_incoming_packet(buffer, callback)
 
-    assert len(callback.call_args_list) == 1
+    ClientFramer().process_incoming_data(buffer, callback)
 
-    response = callback.call_args_list[0][0][0]
+    assert len(callback.call_args_list) == 2
+
+    callback_args = callback.call_args_list[0][0][0]
+    response = callback_args[0]
     assert isinstance(response, ReadInputRegistersResponse)
     assert response.base_register == 0
     assert response.register_count == 120
     assert response.register_values == []
     assert response.error is True
-    assert response.function_code == 0x04
+    assert response.inner_function_code == 0x04
+    assert callback_args[1] == EXCEPTION_RESPONSE_FRAME
 
-    response = callback.call_args_list[1][0][0]
+    callback_args = callback.call_args_list[1][0][0]
+    response = callback_args[0]
     assert isinstance(response, ReadInputRegistersResponse)
     assert response.base_register == 0
     assert response.register_count == 6
     assert response.register_values == [1, 3054, 3029, 3881, 0, 2389]
     assert response.error is False
-    assert response.function_code == 0x04
+    assert response.inner_function_code == 0x04
+    assert callback_args[1] == VALID_RESPONSE_FRAME
